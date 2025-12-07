42646e7677a42f804e51a8947429a585b4ddb7c0
Klimondius <alekseev_ka_1025@1511.ru>
2025-12-08 01:19:47 +0300

    Update settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -1,3 +1,4 @@
+
 # server/config/settings.py
 from pydantic_settings import BaseSettings
 from typing import Optional
@@ -11,11 +12,11 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     # БД
-    POSTGRES_USER: str = "klim"
-    POSTGRES_PASSWORD: str = '6861'
-    POSTGRES_HOST: str = "localhost"
+    POSTGRES_USER: str = "user"
+    POSTGRES_PASSWORD: str = 'J73ZyKfifH8x1AB4kYuNWvrQ9K6OA5VZ'
+    POSTGRES_HOST: str = "dpg-d4iurf15pdvs7385ojl0-a"
     POSTGRES_PORT: int = 5432
-    POSTGRES_DB: str = "Industrial-Site-Design-System"
+    POSTGRES_DB: str = "industrial_site_design_system"
 
     @property
     def DATABASE_URL(self) -> str:
@@ -23,5 +24,6 @@ class Settings(BaseSettings):
             f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
             f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
+
 
 settings = Settings()
