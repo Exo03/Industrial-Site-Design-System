c68d33eafbb38761eaa3e8a322496cf87303ec10
Klimondius <alekseev_ka_1025@1511.ru>
2025-12-07 23:47:47 +0300

    Update __init__.py

--- server/db/models/__init__.py
+++ server/db/models/__init__.py
@@ -1,5 +1,6 @@
-from db.models.user import User
-from db.models.element import Element
-from db.models.project import Project
+from server.db.models.user import User
+from server.db.models.element import Element
+from server.db.models.project import Project
+
 
 __all__ = ["User", "Element", "Project"]
