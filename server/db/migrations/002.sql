aa403e99dae18163d8a6ddf438b14c6950dd5257
Klim <alekseev_ka_1025@1511.ru>
2026-03-17 21:30:45 +0300

    fixed problems

--- server/db/migrations/002.sql
+++ server/db/migrations/002.sql
@@ -1,8 +1,8 @@
 start transaction ;
 
 create table project_members (
-    project_id int not null ,
-    user_id int not null ,
+    project_id int references projects(id) ,
+    user_id int references users(id) ,
     primary key (
         project_id ,
         user_id
--- server/db/models/project_member.py
+++ server/db/models/project_member.py
@@ -5,8 +5,8 @@ from server.db.base import Base
 class ProjectMember(Base):
     __tablename__ = "project_members"
 
-    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    project_id = Column(Integer, ForeignKey("project.id"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    project_id = Column(Integer, ForeignKey("projects.id"), primary_key=True)
 
     project = relationship("Project", back_populates="members")
     user = relationship("User", back_populates="project_memberships")
